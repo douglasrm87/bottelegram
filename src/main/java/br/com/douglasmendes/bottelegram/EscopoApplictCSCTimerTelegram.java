644242ce9f6e63888605c09ca84ef9b123e53e55
Your Name <you@example.com>
2020-03-27 11:00:53 -0300

    BOT na web

--- src/main/java/br/com/douglasmendes/bottelegram/EscopoApplictCSCTimerTelegram.java
+++ src/main/java/br/com/douglasmendes/bottelegram/EscopoApplictCSCTimerTelegram.java
@@ -1,36 +1,37 @@
 package br.com.douglasmendes.bottelegram;
 
 import java.io.Serializable;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 
-//@ApplicationScoped
-//	@PostConstruct
-//@ManagedBean(name = "telegram"/* , eager = true */)
-
+@ApplicationScoped
+@ManagedBean(name = "telegram", eager = true)
 public class EscopoApplictCSCTimerTelegram implements Serializable {
 	private int delay = 4000; // delay de 4 seg.
 	private int interval = 4000; // intervalo de 4 seg.
-//	private FluxoTelegram webTelegram = new FluxoTelegram();
+	private FluxoTelegram webTelegram = new FluxoTelegram();
 
-
-
-	/*
-	 * public void iniciarOuvinteTelegram() {
-	 * System.out.println("Iniciando CSC BOT Telegram."); Timer timer = new Timer();
-	 * timer.scheduleAtFixedRate(new TimerTask() { public void run() {
-	 * EscopoApplictCSCTimerTelegram.this.webTelegram.iniciarChatBotTelegram(); } },
-	 * this.delay, this.interval); }
-	 */
-	
+	@PostConstruct
+	public void iniciarOuvinteTelegram() {
+		System.out.println("Iniciando CSC BOT Telegram.");
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+			public void run() {
+				EscopoApplictCSCTimerTelegram.this.webTelegram.iniciarChatBotTelegram();
+			}
+		}, this.delay, this.interval);
+	}
 
 	public static void main(String[] args) {
 		new EscopoApplictCSCTimerTelegram().processar();
 	}
 
 	private void processar() {
-//		iniciarOuvinteTelegram();
+		iniciarOuvinteTelegram();
 	}
 
 	public EscopoApplictCSCTimerTelegram() {
