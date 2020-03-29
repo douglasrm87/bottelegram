b41b9d607b1e4e7af8dda1913c1e7b5d6d7130bb
Your Name <you@example.com>
2020-03-29 11:51:31 -0300

    .

--- src/main/java/projetojsfmaven/Teste.java
+++ src/main/java/projetojsfmaven/Teste.java
@@ -4,7 +4,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 //http://localhost:8080/projetojsf/index.jsf
 @RequestScoped
-@ManagedBean (name="p")
+@ManagedBean (name="tst")
 public class Teste {
 	private String texto;
 
