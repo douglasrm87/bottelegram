730531fb8e64a94a2fc1458c5aa94346f848314d
Your Name <you@example.com>
2020-03-30 07:51:18 -0300

    .

--- src/main/java/br/com/douglasmendes/bottelegram/EscopoApplictCSCTimerTelegram.java
+++ src/main/java/br/com/douglasmendes/bottelegram/EscopoApplictCSCTimerTelegram.java
@@ -2,7 +2,6 @@ package br.com.douglasmendes.bottelegram;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
