e37d009ca68b911d6735a874f9fbe2f57feb2741
Your Name <you@example.com>
2020-03-30 13:33:16 -0300

    excel

---
+++ src/main/java/br/com/douglasmendes/bottelegram/comando/dto/VPNConectadaExcel.java
@@ -0,0 +1,70 @@
+package br.com.douglasmendes.bottelegram.comando.dto;
+
+public class VPNConectadaExcel {
+	private String time;
+	private String blade;
+	private String severity;
+	private String origin;
+	private String source;
+	private String user;
+	private String destination;
+	private String service;
+	private String id;
+	public String getTime() {
+		return this.time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getBlade() {
+		return this.blade;
+	}
+	public void setBlade(String blade) {
+		this.blade = blade;
+	}
+	public String getSeverity() {
+		return this.severity;
+	}
+	public void setSeverity(String severity) {
+		this.severity = severity;
+	}
+	public String getOrigin() {
+		return this.origin;
+	}
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+	public String getSource() {
+		return this.source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getUser() {
+		return this.user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getDestination() {
+		return this.destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public String getService() {
+		return this.service;
+	}
+	public void setService(String service) {
+		this.service = service;
+	}
+	public String getId() {
+		return this.id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+ 
+	
+	
+}
---
+++ src/main/java/br/com/douglasmendes/bottelegram/excel/PlanilhaExcelVPNConectada.java
@@ -0,0 +1,18 @@
+package br.com.douglasmendes.bottelegram.excel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.douglasmendes.bottelegram.comando.dto.VPNConectadaExcel;
+
+public class PlanilhaExcelVPNConectada {
+	public static void main(String[] args) {
+		
+	}
+	List<VPNConectadaExcel> listaexcel = new ArrayList<>();
+	private void processar() {
+		
+		
+	}
+
+}
