b94b33fea98c433ba1cde3e2b58bd64a3acde312
douglasrm87 <61594551+douglasrm87@users.noreply.github.com>
2024-01-22 14:34:38 -0300

    Update UsoEmail.java

--- src/main/java/br/com/bottelegram/comando/UsoEmail.java
+++ src/main/java/br/com/bottelegram/comando/UsoEmail.java
@@ -1,8 +1,8 @@
 package br.com.bottelegram.comando;
 
 public class UsoEmail {
-	protected static final String REMETENDETE = "douglas.mendes@copel.com";
-	protected static final String CC = "ana.kramer@copel.com";
+	protected static final String REMETENDETE = "douglas.freire@teste.com";
+	protected static final String CC = "ana.maria@teste.com";
 	protected static final String CORPO0 = "<b>Links com orientações</b>";
 
 }
